c491c9e12b7685a3544a66ab4f504a5086cc89ee
dropkun <junya111226@gmail.com>
2024-02-03 02:28:15 +0900

    discord

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::collections::HashMap;
 use std::env;
 
 use dotenv::dotenv;
-use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use serenity::{async_trait, client};
 
 use reqwest;
 use std::time::Duration;
-use tokio::time::sleep;
+use tokio::time;
 
 struct Handler;
 
@@ -21,82 +21,141 @@ impl EventHandler for Handler {
     // Event handlers are dispatched through a threadpool, and so multiple events can be dispatched
     // simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an authentication error, or lack
-            // of permissions to post in the channel, so log to stdout when some error happens,
-            // with a description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {why:?}");
+        let mut map = HashMap::new();
+        map.insert("name", "palworld1");
+        map.insert("project", "droprealms");
+        map.insert("zone", "asia-northeast1-b");
+        match msg.content.as_str() {
+            "!ping" => {
+                if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                    println!("Error sending message: {:?}", why);
+                }
             }
-        } else if msg.content == "!dice" {
-            let random = rand::random::<i32>();
-            let res = random % 100 + 1;
-            if let Err(why) = msg
-                .channel_id
-                .say(&ctx.http, format!("{}", res.abs()))
-                .await
-            {
-                println!("Error sending message: {why:?}");
+            "!dice" => {
+                let res = rand::random::<i32>() % 100 + 1;
+                if let Err(why) = msg.channel_id.say(&ctx.http, res.to_string()).await {
+                    println!("Error sending message: {:?}", why);
+                }
             }
-        } else if msg.content == "!pw start" {
-            let url = env::var("GCP_API").expect("Expected a token in the environment")
-                + "/startinstance";
-            let mut map = HashMap::new();
-            map.insert("name", "palworld1");
-            map.insert("project", "droprealms");
-            map.insert("zone", "asia-northeast1-b");
-            let client = reqwest::Client::new();
-            client.put(url).json(&map).send().await.unwrap();
-            if let Err(why) = msg
-                .channel_id
-                .say(&ctx.http, format!("palworldのサーバーを起動しました。"))
-                .await
-            {
-                println!("Error sending message: {why:?}");
+            "!pw start" => {
+                if let Err(why) = start_mc_server(&ctx, &msg, &map).await {
+                    println!("Error starting server: {:?}", why);
+                }
             }
-        } else if msg.content == "!pw ip" {
-            let url = env::var("GCP_API").expect("Expected a token in the environment") + "/natip ";
-            let mut map = HashMap::new();
-            map.insert("name", "palworld1");
-            map.insert("project", "droprealms");
-            map.insert("zone", "asia-northeast1-b");
-            let client = reqwest::Client::new();
-            let response = client.put(url).json(&map).send().await.unwrap();
-            let body = response.text().await.unwrap();
-            if let Err(why) = msg
-                .channel_id
-                .say(&ctx.http, format!("{}", body + ":8211"))
-                .await
-            {
-                println!("Error sending message: {why:?}");
+            "!pw stop" => {
+                if let Err(why) = stop_mc_server(&ctx, &msg, &map).await {
+                    println!("Error stopping server: {:?}", why);
+                }
             }
-        } else if msg.content == "!pw stop" {
-            let url =
-                env::var("GCP_API").expect("Expected a token in the environment") + "/stopinstance";
-            let mut map = HashMap::new();
-            map.insert("name", "palworld1");
-            map.insert("project", "droprealms");
-            map.insert("zone", "asia-northeast1-b");
-            let client = reqwest::Client::new();
-            client.put(url).json(&map).send().await.unwrap();
-            if let Err(why) = msg
-                .channel_id
-                .say(&ctx.http, format!("palworldのサーバーを停止しました。"))
-                .await
-            {
-                println!("Error sending message: {why:?}");
-            }
+            _ => {}
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a shard is booted, and
-    // a READY payload is sent by Discord. This payload contains data like the current user's guild
-    // Ids, current user data, private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
+}
+
+fn env_var(name: &str) -> String {
+    env::var(name).expect("Expected a token in the environment")
+}
+
+async fn start_mc_server(
+    ctx: &Context,
+    msg: &Message,
+    map: &HashMap<&str, &str>,
+) -> Result<(), reqwest::Error> {
+    let url = env_var("GCP_API");
+    let client = reqwest::Client::new();
+
+    client
+        .post(format!("{}/instance/start", url))
+        .json(&map)
+        .send()
+        .await?;
+
+    if let Err(why) = msg
+        .channel_id
+        .say(&ctx.http, "palworldのサーバーを起動中...")
+        .await
+    {
+        println!("Error sending message: {:?}", why);
+    }
+
+    wait_status("RUNNING".to_string(), &map).await;
+
+    let ip = get_instance_ip(&map).await?;
+
+    if let Err(why) = msg
+        .channel_id
+        .say(&ctx.http, format!("起動が完了しました。{}", ip + ":8211"))
+        .await
+    {
+        println!("Error sending message: {:?}", why);
+    }
+
+    Ok(())
+}
+
+async fn stop_mc_server(
+    ctx: &Context,
+    msg: &Message,
+    map: &HashMap<&str, &str>,
+) -> Result<(), reqwest::Error> {
+    let url = env_var("GCP_API");
+    let client = reqwest::Client::new();
+
+    client
+        .post(format!("{}/instance/stop", url))
+        .json(&map)
+        .send()
+        .await?;
+
+    if let Err(why) = msg
+        .channel_id
+        .say(&ctx.http, "palworldのサーバーを停止中...")
+        .await
+    {
+        println!("Error sending message: {:?}", why);
+    }
+
+    wait_status("TERMINATED".to_string(), &map).await;
+
+    if let Err(why) = msg.channel_id.say(&ctx.http, "停止が完了しました。").await {
+        println!("Error sending message: {:?}", why);
+    }
+
+    Ok(())
+}
+
+async fn wait_status(status: String, map: &HashMap<&str, &str>) {
+    let client = reqwest::Client::new();
+    loop {
+        let url = env_var("GCP_API");
+        let response = client
+            .post(format!("{}/instance/status", url))
+            .json(&map)
+            .send()
+            .await
+            .unwrap();
+
+        let body = response.text().await.unwrap();
+        if body == status {
+            break;
+        }
+        time::sleep(Duration::from_secs(5)).await;
+    }
+}
+
+async fn get_instance_ip(map: &HashMap<&str, &str>) -> Result<String, reqwest::Error> {
+    let url = env_var("GCP_API");
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{}/instance/ip", url))
+        .json(&map)
+        .send()
+        .await?;
+    Ok(response.text().await?)
 }
 
 #[tokio::main]
